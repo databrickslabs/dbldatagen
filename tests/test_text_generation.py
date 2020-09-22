cd647613f9f5334f10354291d3ccd4367b803a78
Ronan Stokes <ronan.stokes@databricks.com>
2020-09-21 17:20:26 -0700

    updates

--- tests/test_text_generation.py
+++ tests/test_text_generation.py
@@ -10,23 +10,22 @@ import unittest
 import re
 import pandas as pd
 
-
 schema = StructType([
-    StructField("PK1",StringType(),True),
-    StructField("LAST_MODIFIED_UTC",TimestampType(),True),
-    StructField("date",DateType(),True),
-    StructField("str1",StringType(),True),
-    StructField("nint",IntegerType(),True),
-    StructField("nstr1",StringType(),True),
-    StructField("nstr2",StringType(),True),
-    StructField("nstr3",StringType(),True),
-    StructField("nstr4",StringType(),True),
-    StructField("nstr5",StringType(),True),
-    StructField("nstr6",StringType(),True),
-    StructField("email",StringType(),True),
-    StructField("ip_addr",StringType(),True),
-    StructField("phone",StringType(),True),
-    StructField("isDeleted",BooleanType(),True)
+    StructField("PK1", StringType(), True),
+    StructField("LAST_MODIFIED_UTC", TimestampType(), True),
+    StructField("date", DateType(), True),
+    StructField("str1", StringType(), True),
+    StructField("nint", IntegerType(), True),
+    StructField("nstr1", StringType(), True),
+    StructField("nstr2", StringType(), True),
+    StructField("nstr3", StringType(), True),
+    StructField("nstr4", StringType(), True),
+    StructField("nstr5", StringType(), True),
+    StructField("nstr6", StringType(), True),
+    StructField("email", StringType(), True),
+    StructField("ip_addr", StringType(), True),
+    StructField("phone", StringType(), True),
+    StructField("isDeleted", BooleanType(), True)
 ])
 
 # add the following if using pandas udfs
@@ -38,11 +37,12 @@ spark = dg.SparkSingleton.getLocalInstance("unit tests")
 spark.conf.set("spark.sql.execution.arrow.maxRecordsPerBatch", "500")
 spark.conf.set("spark.sql.execution.arrow.enabled", "true")
 
+
 # Test manipulation and generation of test data for a large schema
 class TestTextGeneration(unittest.TestCase):
     testDataSpec = None
     row_count = 1000000
-    partitions_requested=24
+    partitions_requested = 24
 
     def setUp(self):
         print("setting up TestTextDataGenerationTests")
@@ -60,14 +60,14 @@ class TestTextGeneration(unittest.TestCase):
                             .withColumnSpec("nstr1", percent_nulls=10.0, min=1, max=9, step=2)
                             .withColumnSpec("nstr2", percent_nulls=10.0, min=1.5, max=2.5, step=0.3, format="%04f")
                             .withColumnSpec("nstr3", min=1.0, max=9.0, step=2.0)
-                            .withColumnSpec("nstr4", percent_nulls=10.0, min=1, max=9, step=2,  format="%04d")
-                            .withColumnSpec("nstr5", percent_nulls=10.0, min=1.5, max=2.5, step=0.3,  random=True)
-                            .withColumnSpec("nstr6", percent_nulls=10.0, min=1.5, max=2.5, step=0.3, random=True, format="%04f")
+                            .withColumnSpec("nstr4", percent_nulls=10.0, min=1, max=9, step=2, format="%04d")
+                            .withColumnSpec("nstr5", percent_nulls=10.0, min=1.5, max=2.5, step=0.3, random=True)
+                            .withColumnSpec("nstr6", percent_nulls=10.0, min=1.5, max=2.5, step=0.3, random=True,
+                                            format="%04f")
                             .withColumnSpec("email", template=r'\w.\w@\w.com|\w@\w.co.u\k')
                             .withColumnSpec("ip_addr", template=r'\n.\n.\n.\n')
                             .withColumnSpec("phone", template=r'(ddd)-ddd-dddd|1(ddd) ddd-dddd|ddd ddddddd')
                             )
-
 
     def test_simple_data(self):
         self.testDataSpec.build().show()
@@ -79,9 +79,9 @@ class TestTextGeneration(unittest.TestCase):
                          .withIdOutput()
                          .withColumnSpec("date", percent_nulls=10.0)
                          .withColumnSpecs(patterns="n.*", match_types=StringType(),
-                                        percent_nulls=10.0, min=1, max=9, step=2)
+                                          percent_nulls=10.0, min=1, max=9, step=2)
                          .withColumnSpecs(patterns="n.*", match_types=IntegerType(),
-                                        percent_nulls=10.0, min=1, max=200, step=-2)
+                                          percent_nulls=10.0, min=1, max=200, step=-2)
                          .withColumnSpec("email", template=r'\w.\w@\w.com|\w@\w.co.u\k')
                          .withColumnSpec("ip_addr", template=r'\n.\n.\n.\n')
                          .withColumnSpec("phone", template=r'(ddd)-ddd-dddd|1(ddd) ddd-dddd|ddd ddddddd')
@@ -94,7 +94,8 @@ class TestTextGeneration(unittest.TestCase):
                          .withIdOutput()
                          .withColumn("val1", IntegerType(), percent_nulls=10.0)
                          .withColumn("val2", IntegerType(), percent_nulls=10.0)
-                         .withColumn("val3", StringType(), base_column=["val1","val2"], base_column_type="values", template=r"\v-1")
+                         .withColumn("val3", StringType(), base_column=["val1", "val2"], base_column_type="values",
+                                     template=r"\v-1")
                          )
 
         testDataSpec3.build().show()
@@ -105,7 +106,8 @@ class TestTextGeneration(unittest.TestCase):
                          .withIdOutput()
                          .withColumn("val1", IntegerType(), percent_nulls=10.0)
                          .withColumn("val2", IntegerType(), percent_nulls=10.0)
-                         .withColumn("val3", StringType(), base_column=["val1","val2"], base_column_type="values", template=r"\v0-\v1")
+                         .withColumn("val3", StringType(), base_column=["val1", "val2"], base_column_type="values",
+                                     template=r"\v0-\v1")
                          )
         # in this case we expect values of the form `<first-value> - <second-value>`
         testDataSpec4.build().show()
@@ -116,7 +118,8 @@ class TestTextGeneration(unittest.TestCase):
                          .withIdOutput()
                          .withColumn("val1", IntegerType(), percent_nulls=10.0)
                          .withColumn("val2", IntegerType(), percent_nulls=10.0)
-                         .withColumn("val3", StringType(), base_column=["val1","val2"], base_column_type="values", template=r"\v\0-\v\1")
+                         .withColumn("val3", StringType(), base_column=["val1", "val2"], base_column_type="values",
+                                     template=r"\v\0-\v\1")
                          )
 
         # in this case we expect values of the form `[ array of values]0 - [array of values]1`
@@ -128,7 +131,8 @@ class TestTextGeneration(unittest.TestCase):
                          .withIdOutput()
                          .withColumn("val1", IntegerType(), percent_nulls=10.0)
                          .withColumn("val2", IntegerType(), percent_nulls=10.0)
-                         .withColumn("val3", StringType(), base_column=["val1","val2"], base_column_type="hash", template=r"\v0-\v1")
+                         .withColumn("val3", StringType(), base_column=["val1", "val2"], base_column_type="hash",
+                                     template=r"\v0-\v1")
                          )
         # here the values for val3 are undefined as the base value for the column is a hash of the base columns
         testDataSpec6.build().show()
@@ -139,7 +143,8 @@ class TestTextGeneration(unittest.TestCase):
                          .withIdOutput()
                          .withColumn("val1", IntegerType(), percent_nulls=10.0)
                          .withColumn("val2", IntegerType(), percent_nulls=10.0)
-                         .withColumn("val3", StringType(), base_column=["val1","val2"], base_column_type="hash", format="%s")
+                         .withColumn("val3", StringType(), base_column=["val1", "val2"], base_column_type="hash",
+                                     format="%s")
                          )
         # here the values for val3 are undefined as the base value for the column is a hash of the base columns
         testDataSpec7.build().show()
@@ -150,7 +155,8 @@ class TestTextGeneration(unittest.TestCase):
                          .withIdOutput()
                          .withColumn("val1", IntegerType(), percent_nulls=10.0)
                          .withColumn("val2", IntegerType(), percent_nulls=10.0)
-                         .withColumn("val3", StringType(), base_column=["val1","val2"], base_column_type="values", format="%s")
+                         .withColumn("val3", StringType(), base_column=["val1", "val2"], base_column_type="values",
+                                     format="%s")
                          )
         # here the values for val3 are undefined as the base value for the column is a hash of the base columns
         testDataSpec8.build().show()
@@ -161,7 +167,7 @@ class TestTextGeneration(unittest.TestCase):
                          .withIdOutput()
                          .withColumn("val1", IntegerType(), percent_nulls=10.0)
                          .withColumn("val2", IntegerType(), percent_nulls=10.0)
-                         .withColumn("val3", StringType(), base_column=["val1","val2"], format="%s")
+                         .withColumn("val3", StringType(), base_column=["val1", "val2"], format="%s")
                          )
         # here the values for val3 are undefined as the base value for the column is a hash of the base columns
         testDataSpec9.build().show()
