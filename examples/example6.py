0df4459bb043f629ccdb1b1f1aafa9e42b6c68cb
Ronan Stokes <ronan.stokes@databricks.com>
2021-08-23 11:15:40 -0700

    code cleanup

--- examples/example6.py
+++ examples/example6.py
@@ -1,10 +1,9 @@
 from datetime import timedelta, datetime
-import math
-from pyspark.sql.types import StructType, StructField, IntegerType, StringType, FloatType, TimestampType
-from dbldatagen import DateRange
-import dbldatagen as dg
-from pyspark.conf import SparkConf
+
 from pyspark.sql import SparkSession
+
+import dbldatagen as dg
+from dbldatagen import DateRange
 
 interval = timedelta(days=1, hours=1)
 start = datetime(2017, 10, 1, 0, 0, 0)
