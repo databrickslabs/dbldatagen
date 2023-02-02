a891d47e44bd6305795f98c3b779f4b57239e6d3
ronanstokes-db <ronan.stokes@databricks.com>
2023-02-02 00:47:46 -0800

    updated tests for alternative seed id names

--- dbldatagen/data_generator.py
+++ dbldatagen/data_generator.py
@@ -714,8 +714,11 @@ class DataGenerator:
             self._checkColumnOrColumnList(baseColumn, allowId=True)
 
         if not noWarn and colName == DEFAULT_SEED_COLUMN and self._seedColumnName == DEFAULT_SEED_COLUMN:
-            self.logger.warning(f"Adding a column named '{colName}' overriding seed column '{self._seedColumnName}'")
-            self.logger.warning(f"Use `seedColumNamen` option on DataGenerator construction for different seed column")
+            self.logger.warning(f"Adding a new column named '{colName}' overrides seed column '{self._seedColumnName}'")
+            self.logger.warning(f"Use `seedColumName` option on DataGenerator construction for different seed column")
+        elif not noWarn and colName == self._seedColumnName:
+            self.logger.warning(f"Adding a new column named '{colName}' overrides seed column '{self._seedColumnName}'")
+            self.logger.warning(f"Use `seedColumName` option on DataGenerator construction with different seed column")
 
         # handle migration of old `min` and `max` options
         if _OLD_MIN_OPTION in kwargs:
