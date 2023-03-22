0f2d6a49fc492fb7e685a9b8c74fc8cfc086c9a3
ronanstokes-db <ronan.stokes@databricks.com>
2023-03-22 13:23:33 -0700

    wip

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ package_long_description = """###Databricks Labs Spark Data Generator###
 
 setuptools.setup(
     name="dbldatagen",
-    version="0.3.3",
+    version="0.3.2post1",
     author="Ronan Stokes, Databricks",
     description="Databricks Labs -  PySpark Synthetic Data Generator",
     long_description=long_description,
