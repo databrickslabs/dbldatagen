c43adb80cd4285948465b0c0c3ed621b5002233b
ronanstokes-db <ronan.stokes@databricks.com>
2023-03-30 01:39:02 -0700

    wip

--- dbldatagen/utils.py
+++ dbldatagen/utils.py
@@ -20,7 +20,6 @@ def deprecated(message=""):
     Note there is a 3rd party library called `deprecated` that provides this feature but goal is to only have
     dependencies on packages already used in the Databricks runtime
     """
-
     # create closure around function that follows use of the decorator
 
     def deprecated_decorator(func):
