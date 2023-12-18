ed989edbd3577359d829166855b0e7812c0a01ef
ronanstokes-db <ronan.stokes@databricks.com>
2023-12-18 04:11:35 -0800

    updates to handle shared sparkSession

--- tests/test_shared_env.py
+++ tests/test_shared_env.py
@@ -1,5 +1,5 @@
 import logging
-from unittest.mock import Mock
+from unittest.mock import Mock, PropertyMock
 
 import pytest
 import dbldatagen as dg
@@ -33,6 +33,16 @@ class TestSharedEnv:
         del spark.sparkContext
         return spark
 
+    @pytest.fixture(scope="class")
+    def sparkSessionNullContext(self, setupLogging):
+
+        class MockSparkSession:
+            def __init__(self):
+                self.sparkContext = None
+
+        spark = MockSparkSession()
+        return spark
+
     def test_getDefaultParallelism(self, sparkSession):
         """Test that the default parallelism is returned when the sparkSession object supports use of the
         sparkContext attribute to get the default parallelism.
@@ -50,9 +60,23 @@ class TestSharedEnv:
         defaultParallelism = dg.DataGenerator._getDefaultSparkParallelism(sharedSparkSession)
         assert defaultParallelism == dg.SPARK_DEFAULT_PARALLELISM
 
+    def test_getNullContextDefaultParallelism(self, sparkSessionNullContext):
+        """Test that the default parallelism is returned when the sparkSession object supports use of the
+        sparkContext attribute to get the default parallelism.
+
+        :param sparkSession: The sparkSession object to use for the test.
+        """
+        defaultParallelism = dg.DataGenerator._getDefaultSparkParallelism(sparkSessionNullContext)
+        assert defaultParallelism == dg.SPARK_DEFAULT_PARALLELISM
+
     def test_mocked_shared_session1(self, sharedSparkSession):
         # validate that accessing the sparkContext on the shared spark session raises an exception
         with pytest.raises(Exception) as excinfo:
             context = sharedSparkSession.sparkContext
 
         assert "sparkContext" in str(excinfo.value)
+
+    def test_null_context_spark_session(self, sparkSessionNullContext):
+        # validate that accessing the sparkContext on the shared spark session raises an exception
+        context = sparkSessionNullContext.sparkContext
+        assert context is None
