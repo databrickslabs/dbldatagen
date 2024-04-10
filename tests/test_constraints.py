a521fb0db196f5331d2b55cc37d9794efaf4ad8d
ronanstokes-db <ronan.stokes@databricks.com>
2024-04-10 12:03:29 -0700

    wip

--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -2,7 +2,6 @@ import logging
 
 import pytest
 from pyspark.sql.types import IntegerType, StringType, FloatType
-import pyspark.sql.functions as F
 
 import dbldatagen as dg
 from dbldatagen.constraints import SqlExpr, LiteralRelation, ChainedRelation, LiteralRange, RangedValues
@@ -18,7 +17,8 @@ def setupLogging():
 
 class TestConstraints:
 
-    def test_simple_constraints(self):
+    @pytest.fixture()
+    def generationSpec1(self):
         testDataSpec = (dg.DataGenerator(sparkSession=spark, name="test_data_set1", rows=1000,
                                          partitions=4)
                         .withIdOutput()
@@ -29,9 +29,14 @@ class TestConstraints:
                         .withColumn("code4", StringType(), values=['a', 'b', 'c'])
                         .withColumn("code5", StringType(), values=['a', 'b', 'c'], random=True)
                         .withColumn("code6", StringType(), values=['a', 'b', 'c'], random=True, weights=[9, 1, 1])
+                        )
+
+        return testDataSpec
+
+    def test_simple_constraints(self, generationSpec1):
+        testDataSpec = (generationSpec1
                         .withSqlConstraint("id < 100")
                         .withSqlConstraint("id > 0")
-
                         )
 
         testDataDF = testDataSpec.build()
@@ -39,17 +44,8 @@ class TestConstraints:
         rowCount = testDataDF.count()
         assert rowCount == 99
 
-    def test_simple_constraints2(self):
-        testDataSpec = (dg.DataGenerator(sparkSession=spark, name="test_data_set1", rows=1000,
-                                         partitions=4)
-                        .withIdOutput()
-                        .withColumn("r", FloatType(), expr="floor(rand() * 350) * (86400 + 3600)")
-                        .withColumn("code1", IntegerType(), unique_values=100)
-                        .withColumn("code2", IntegerType(), min=1, max=200)
-                        .withColumn("code3", IntegerType(), maxValue=10)
-                        .withColumn("code4", StringType(), values=['a', 'b', 'c'])
-                        .withColumn("code5", StringType(), values=['a', 'b', 'c'], random=True)
-                        .withColumn("code6", StringType(), values=['a', 'b', 'c'], random=True, weights=[9, 1, 1])
+    def test_simple_constraints2(self, generationSpec1):
+        testDataSpec = (generationSpec1
                         .withConstraint(SqlExpr("id < 100"))
                         )
 
@@ -58,20 +54,10 @@ class TestConstraints:
         rowCount = testDataDF.count()
         assert rowCount == 100
 
-    def test_multiple_constraints(self):
-        testDataSpec = (dg.DataGenerator(sparkSession=spark, name="test_data_set1", rows=1000,
-                                         partitions=4)
-                        .withIdOutput()
-                        .withColumn("r", FloatType(), expr="floor(rand() * 350) * (86400 + 3600)")
-                        .withColumn("code1", IntegerType(), min=1, max=100, random=True)
-                        .withColumn("code2", IntegerType(), min=1, max=200)
-                        .withColumn("code3", IntegerType(), maxValue=10)
-                        .withColumn("code4", StringType(), values=['a', 'b', 'c'])
-                        .withColumn("code5", StringType(), values=['a', 'b', 'c'], random=True)
-                        .withColumn("code6", StringType(), values=['a', 'b', 'c'], random=True, weights=[9, 1, 1])
+    def test_multiple_constraints(self, generationSpec1):
+        testDataSpec = (generationSpec1
                         .withConstraints([SqlExpr("id < 100"),
                                           SqlExpr("id > 0")])
-
                         )
 
         testDataDF = testDataSpec.build()
@@ -79,17 +65,8 @@ class TestConstraints:
         rowCount = testDataDF.count()
         assert rowCount == 99
 
-    def test_scalar_relation(self):
-        testDataSpec = (dg.DataGenerator(sparkSession=spark, name="test_data_set1", rows=1000,
-                                         partitions=4)
-                        .withIdOutput()
-                        .withColumn("r", FloatType(), expr="floor(rand() * 350) * (86400 + 3600)")
-                        .withColumn("code1", IntegerType(), min=1, max=100, random=True)
-                        .withColumn("code2", IntegerType(), min=1, max=200)
-                        .withColumn("code3", IntegerType(), maxValue=10)
-                        .withColumn("code4", StringType(), values=['a', 'b', 'c'])
-                        .withColumn("code5", StringType(), values=['a', 'b', 'c'], random=True)
-                        .withColumn("code6", StringType(), values=['a', 'b', 'c'], random=True, weights=[9, 1, 1])
+    def test_scalar_relation(self, generationSpec1):
+        testDataSpec = (generationSpec1
                         .withConstraints([SqlExpr("id < 100"),
                                           SqlExpr("id > 0")])
                         .withConstraint(LiteralRelation("id", "<", 50))
@@ -100,18 +77,9 @@ class TestConstraints:
         rowCount = testDataDF.count()
         assert rowCount == 49
 
-    def test_scalar_relation_bad(self):
+    def test_scalar_relation_bad(self, generationSpec1):
         with pytest.raises(ValueError):
-            testDataSpec = (dg.DataGenerator(sparkSession=spark, name="test_data_set1", rows=1000,
-                                             partitions=4)
-                            .withIdOutput()
-                            .withColumn("r", FloatType(), expr="floor(rand() * 350) * (86400 + 3600)")
-                            .withColumn("code1", IntegerType(), min=1, max=100, random=True)
-                            .withColumn("code2", IntegerType(), min=1, max=200)
-                            .withColumn("code3", IntegerType(), maxValue=10)
-                            .withColumn("code4", StringType(), values=['a', 'b', 'c'])
-                            .withColumn("code5", StringType(), values=['a', 'b', 'c'], random=True)
-                            .withColumn("code6", StringType(), values=['a', 'b', 'c'], random=True, weights=[9, 1, 1])
+            testDataSpec = (generationSpec1
                             .withConstraints([SqlExpr("id < 100"),
                                               SqlExpr("id > 0")])
                             .withConstraint(LiteralRelation("id", "<<<", 50))
@@ -122,7 +90,8 @@ class TestConstraints:
             rowCount = testDataDF.count()
             assert rowCount == 49
 
-    def test_chained_relation(self):
+    @pytest.fixture()
+    def generationSpec2(self):
         testDataSpec = (dg.DataGenerator(sparkSession=spark, name="test_data_set1", rows=1000,
                                          partitions=4)
                         .withIdOutput()
@@ -130,6 +99,12 @@ class TestConstraints:
                         .withColumn("code1", IntegerType(), min=1, max=100)
                         .withColumn("code2", IntegerType(), min=50, max=150)
                         .withColumn("code3", IntegerType(), min=100, max=200)
+                        )
+
+        return testDataSpec
+
+    def test_chained_relation(self, generationSpec2):
+        testDataSpec = (generationSpec2
                         .withConstraints([SqlExpr("id < 100"),
                                           SqlExpr("id > 0")])
                         .withConstraint(ChainedRelation(["code1", "code2", "code3"], "<"))
@@ -140,14 +115,8 @@ class TestConstraints:
         rowCount = testDataDF.count()
         assert rowCount == 99
 
-    def test_chained_relation2(self):
-        testDataSpec = (dg.DataGenerator(sparkSession=spark, name="test_data_set1", rows=1000,
-                                         partitions=4)
-                        .withIdOutput()
-                        .withColumn("r", FloatType(), expr="floor(rand() * 350) * (86400 + 3600)")
-                        .withColumn("code1", IntegerType(), min=1, max=100)
-                        .withColumn("code2", IntegerType(), min=50, max=150)
-                        .withColumn("code3", IntegerType(), min=100, max=200)
+    def test_chained_relation2(self, generationSpec2):
+        testDataSpec = (generationSpec2
                         .withConstraints([SqlExpr("id < 100"),
                                           SqlExpr("id > 0")])
                         .withConstraint(ChainedRelation(["code1", "code2", "code3"], "<="))
@@ -158,14 +127,8 @@ class TestConstraints:
         rowCount = testDataDF.count()
         assert rowCount == 99
 
-    def test_chained_relation3(self):
-        testDataSpec = (dg.DataGenerator(sparkSession=spark, name="test_data_set1", rows=1000,
-                                         partitions=4)
-                        .withIdOutput()
-                        .withColumn("r", FloatType(), expr="floor(rand() * 350) * (86400 + 3600)")
-                        .withColumn("code1", IntegerType(), min=1, max=100)
-                        .withColumn("code2", IntegerType(), min=50, max=150)
-                        .withColumn("code3", IntegerType(), min=100, max=200)
+    def test_chained_relation3(self, generationSpec2):
+        testDataSpec = (generationSpec2
                         .withConstraints([SqlExpr("id < 100"),
                                           SqlExpr("id > 0")])
                         .withConstraint(ChainedRelation(["code3", "code2", "code1"], ">"))
@@ -176,14 +139,8 @@ class TestConstraints:
         rowCount = testDataDF.count()
         assert rowCount == 99
 
-    def test_chained_relation4(self):
-        testDataSpec = (dg.DataGenerator(sparkSession=spark, name="test_data_set1", rows=1000,
-                                         partitions=4)
-                        .withIdOutput()
-                        .withColumn("r", FloatType(), expr="floor(rand() * 350) * (86400 + 3600)")
-                        .withColumn("code1", IntegerType(), min=1, max=100)
-                        .withColumn("code2", IntegerType(), min=50, max=150)
-                        .withColumn("code3", IntegerType(), min=100, max=200)
+    def test_chained_relation4(self, generationSpec2):
+        testDataSpec = (generationSpec2
                         .withConstraints([SqlExpr("id < 100"),
                                           SqlExpr("id > 0")])
                         .withConstraint(ChainedRelation(["code3", "code2", "code1"], ">="))
@@ -194,15 +151,9 @@ class TestConstraints:
         rowCount = testDataDF.count()
         assert rowCount == 99
 
-    def test_chained_relation_bad(self):
+    def test_chained_relation_bad(self, generationSpec2):
         with pytest.raises(ValueError):
-            testDataSpec = (dg.DataGenerator(sparkSession=spark, name="test_data_set1", rows=1000,
-                                             partitions=4)
-                            .withIdOutput()
-                            .withColumn("r", FloatType(), expr="floor(rand() * 350) * (86400 + 3600)")
-                            .withColumn("code1", IntegerType(), min=1, max=100)
-                            .withColumn("code2", IntegerType(), min=50, max=150)
-                            .withColumn("code3", IntegerType(), min=100, max=200)
+            testDataSpec = (generationSpec2
                             .withConstraints([SqlExpr("id < 100"),
                                               SqlExpr("id > 0")])
                             .withConstraint(ChainedRelation(["code3", "code2", "code1"], ">>>"))
@@ -213,14 +164,8 @@ class TestConstraints:
             rowCount = testDataDF.count()
             assert rowCount == 99
 
-    def test_literal_range(self):
-        testDataSpec = (dg.DataGenerator(sparkSession=spark, name="test_data_set1", rows=1000,
-                                         partitions=4)
-                        .withIdOutput()
-                        .withColumn("r", FloatType(), expr="floor(rand() * 350) * (86400 + 3600)")
-                        .withColumn("code1", IntegerType(), min=1, max=100)
-                        .withColumn("code2", IntegerType(), min=50, max=150)
-                        .withColumn("code3", IntegerType(), min=100, max=200)
+    def test_literal_range(self, generationSpec2):
+        testDataSpec = (generationSpec2
                         .withConstraints([SqlExpr("id < 100"),
                                           SqlExpr("id > 0")])
                         .withConstraint(LiteralRange("id", 0, 100, strict=True))
@@ -231,14 +176,8 @@ class TestConstraints:
         rowCount = testDataDF.count()
         assert rowCount == 99
 
-    def test_ranged_values(self):
-        testDataSpec = (dg.DataGenerator(sparkSession=spark, name="test_data_set1", rows=1000,
-                                         partitions=4)
-                        .withIdOutput()
-                        .withColumn("r", FloatType(), expr="floor(rand() * 350) * (86400 + 3600)")
-                        .withColumn("code1", IntegerType(), min=1, max=100)
-                        .withColumn("code2", IntegerType(), min=50, max=150)
-                        .withColumn("code3", IntegerType(), min=100, max=200)
+    def test_ranged_values(self, generationSpec2):
+        testDataSpec = (generationSpec2
                         .withConstraints([SqlExpr("id < 100"),
                                           SqlExpr("id > 0")])
                         .withConstraint(RangedValues("code2", "code1", "code3", strict=True))
