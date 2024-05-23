c6352dc4c678311310ba0e0b5aa489176234b231
ronanstokes-db <ronan.stokes@databricks.com>
2024-05-23 16:18:43 -0700

    wip

--- dbldatagen/distributions/__init__.py
+++ dbldatagen/distributions/__init__.py
@@ -19,15 +19,15 @@ This module defines the package contents for the test data generator library dis
 """
 
 
-from .data_distribution import DataDistribution
 from .normal_distribution import Normal
+from .data_distribution import DataDistribution
 from .beta import Beta
 from .gamma import Gamma
 from .exponential_distribution import Exponential
 
 
-__all__ = ["data_distribution",
-           "normal_distribution",
+__all__ = ["normal_distribution",
            "gamma",
            "beta",
+           "data_distribution",
            "exponential_distribution"]
--- dbldatagen/distributions/data_distribution.py
+++ dbldatagen/distributions/data_distribution.py
@@ -20,9 +20,8 @@ Some data distributions are scaled to the [0,1] interval as part of their data g
 and no further scaling is needed.
 """
 import copy
-
-import numpy as np
 import pyspark.sql.functions as F
+import numpy as np
 
 
 class DataDistribution(object):
