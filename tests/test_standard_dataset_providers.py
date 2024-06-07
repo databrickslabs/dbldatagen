4206b5c1ad027ef58689d2ecd09c6cdd0efb688a
ghanse <163584195+ghanse@users.noreply.github.com>
2024-06-07 16:36:18 -0400

    Feature standard datasets - part 2 (#286)
    * work in progress
    
    * work in progress
    
    * work in progress
    
    * wip
    
    * wip
    
    * added implementations for Datasets describe and listing
    
    * bumpedBuild
    
    * bumpedBuild
    
    * bumpedBuild
    
    * bumpedBuild
    
    * fixed dataset provider imports
    
    * fixed dataset provider imports
    
    * fixed dataset provider imports
    
    * fixed dataset provider imports
    
    * fixed dataset provider imports
    
    * fixed dataset provider imports
    
    * wip
    
    * wip
    
    * initial working version
    
    * initial working version
    
    * initial working version
    
    * initial working version
    
    * initial working version
    
    * initial working version
    
    * initial working version
    
    * initial working version
    
    * initial working version
    
    * added telephony plans
    
    * added telephony plans
    
    * added telephony plans
    
    * initial working version
    added plugin mechanics, initial user table and part of telephony plans
    
    * Added tokei.rs badge (#253)
    
    [![lines of code](https://tokei.rs/b1/github/databrickslabs/dbldatagen)]([https://codecov.io/github/databrickslabs/dbldatagen](https://github.com/databrickslabs/dbldatagen))
    
    * Prep for release 036 (#251)
    
    * prep for version 0.3.6
    
    * added telephony plans
    
    * initial implementation
    
    * added basic/iot dataset
    
    * wip
    
    * work in progress
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * work in progress
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * additional coverage tests
    
    * additional coverage
    
    * additional coverage
    
    * additional coverage
    
    * additional coverage
    
    * Add some standard datasets
    
    * Fix a bug with the default number of groups
    
    * Test changes
    
    * Updated datasets
    
    * Added more unit tests and safety
    
    * Added more tests
    
    * Update test_standard_dataset_providers.py
    
    added space to line 147 to pass lint checks
    
    ---------
    
    Co-authored-by: ronanstokes-db <ronan.stokes@databricks.com>
    Co-authored-by: Ronan Stokes <42389040+ronanstokes-db@users.noreply.github.com>
    Co-authored-by: Serge Smertin <259697+nfx@users.noreply.github.com>

--- tests/test_standard_dataset_providers.py
+++ tests/test_standard_dataset_providers.py
@@ -6,7 +6,210 @@ spark = dg.SparkSingleton.getLocalInstance("unit tests")
 
 
 class TestStandardDatasetProviders:
+    
+    # BASIC GEOMETRIES tests:
+    @pytest.mark.parametrize("providerName, providerOptions", [
+        ("basic/geometries", 
+            {"rows": 50, "partitions": 4, "random": False, "geometryType": "point", "maxVertices": 1}),
+        ("basic/geometries", 
+            {"rows": 100, "partitions": -1, "random": False, "geometryType": "point", "maxVertices": 2}),
+        ("basic/geometries", 
+            {"rows": -1, "partitions": 4, "random": True, "geometryType": "point"}),
+        ("basic/geometries", {}),
+        ("basic/geometries", 
+            {"rows": 5000, "partitions": -1, "random": True, "geometryType": "lineString"}),
+        ("basic/geometries", 
+            {"rows": -1, "partitions": -1, "random": False,  "geometryType": "lineString", "maxVertices": 2}),
+        ("basic/geometries", 
+            {"rows": -1, "partitions": 4, "random": True,  "geometryType": "lineString", "maxVertices": 1}),
+        ("basic/geometries", 
+            {"rows": 5000, "partitions": 4, "geometryType": "lineString", "maxVertices": 2}),
+        ("basic/geometries", 
+            {"rows": 5000, "partitions": -1, "random": False, "geometryType": "polygon"}),
+        ("basic/geometries", 
+            {"rows": -1, "partitions": -1, "random": True,  "geometryType": "polygon", "maxVertices": 3}),
+        ("basic/geometries", 
+            {"rows": -1, "partitions": 4, "random": True,  "geometryType": "polygon", "maxVertices": 2}),
+        ("basic/geometries", 
+            {"rows": 5000, "partitions": 4, "geometryType": "polygon", "maxVertices": 5}),
+    ])
+    def test_basic_geometries_retrieval(self, providerName, providerOptions):
+        ds = dg.Datasets(spark, providerName).get(**providerOptions)
+        assert ds is not None
 
+        df = ds.build()
+        assert df.count() >= 0
+        assert "wkt" in df.columns
+
+        geometryType = providerOptions.get("geometryType", None)
+        row = df.first().asDict()
+        if geometryType == "point" or geometryType is None:
+            assert "POINT" in row["wkt"]
+
+        if geometryType == "lineString":
+            assert "LINESTRING" in row["wkt"]
+
+        if geometryType == "polygon":
+            assert "POLYGON" in row["wkt"]
+
+        random = providerOptions.get("random", None)
+        if random:
+            print("")
+            leadingRows = df.limit(100).collect()
+            ids = [r.location_id for r in leadingRows]
+            assert ids != sorted(ids)
+
+    # BASIC PROCESS HISTORIAN tests:
+    @pytest.mark.parametrize("providerName, providerOptions", [
+        ("basic/process_historian", 
+            {"rows": 50, "partitions": 4, "random": False, "numDevices": 1, "numPlants": 1,
+            "numTags": 1, "startTimestamp": "2020-01-01 00:00:00", "endTimestamp": "2020-04-01 00:00:00"}),
+        ("basic/process_historian", 
+            {"rows": 1000, "partitions": -1, "random": True, "numDevices": 10, "numPlants": 2,
+            "numTags": 2, "startTimestamp": "2020-01-01 00:00:00", "endTimestamp": "2020-04-01 00:00:00"}),
+        ("basic/process_historian", 
+            {"rows": 5000, "partitions": -1, "random": True, "numDevices": 100, "numPlants": 10,
+            "numTags": 5, "startTimestamp": "2020-01-01 00:00:00", "endTimestamp": "2020-04-01 00:00:00"}),
+        ("basic/process_historian", {}),
+        ("basic/process_historian", 
+            {"rows": 5000, "partitions": -1, "random": True, "numDevices": 100, "numPlants": 10,
+            "numTags": 5, "startTimestamp": "2020-04-01 00:00:00", "endTimestamp": "2020-01-01 00:00:00"}),
+        ("basic/process_historian", 
+            {"rows": 100, "partitions": -1, "random": True, "numDevices": 100, "numPlants": 10,
+            "numTags": 5, "startTimestamp": "2020-01-01 00:00:00", "endTimestamp": "2020-04-01 00:00:00"}),
+        ("basic/process_historian",
+            {"rows": 100, "partitions": -1, "random": True, "numDevices": 100, "numPlants": 10,
+            "numTags": 5, "startTimestamp": "2020-01-01 00:00:00", "endTimestamp": "2020-04-01 00:00:00",
+            "dataQualityRatios": {"pctQuestionable": 0.1, "pctAnnotated": 0.05, "pctSubstituded": 0.12}}),
+        ("basic/process_historian",
+            {"rows": 100, "partitions": -1, "random": True, "numDevices": 100, "numPlants": 10,
+            "numTags": 5, "startTimestamp": "2020-01-01 00:00:00", "endTimestamp": "2020-04-01 00:00:00",
+            "dataQualityRatios": {"pctQuestionable": 0.1, "pctSubstituded": 0.12}}),
+        ("basic/process_historian",
+            {"rows": 100, "partitions": -1, "random": True, "numDevices": 100, "numPlants": 10,
+            "numTags": 5, "startTimestamp": "2020-01-01 00:00:00", "endTimestamp": "2020-04-01 00:00:00",
+            "dataQualityRatios": {"pctAnnotated": 0.05}}),
+
+    ])
+    def test_basic_process_historian_retrieval(self, providerName, providerOptions):
+        ds = dg.Datasets(spark, providerName).get(**providerOptions)
+        assert ds is not None
+
+        df = ds.build()
+        assert df.count() >= 0
+        
+        startTimestamp = providerOptions.get("startTimestamp", "2024-01-01 00:00:00")
+        endTimestamp = providerOptions.get("endTimestamp", "2024-02-01 00:00:00")
+        if startTimestamp > endTimestamp:
+            (startTimestamp, endTimestamp) = (endTimestamp, startTimestamp)
+        assert df.where(f'ts < "{startTimestamp}"').count() == 0
+        assert df.where(f'ts > "{endTimestamp}"').count() == 0
+
+        random = providerOptions.get("random", None)
+        if random:
+            print("")
+            leadingRows = df.limit(100).collect()
+            ids = [r.device_id for r in leadingRows]
+            assert ids != sorted(ids)
+
+    # BASIC TELEMATICS tests:
+    @pytest.mark.parametrize("providerName, providerOptions", [
+        ("basic/telematics", 
+            {"rows": 50, "partitions": 4, "random": False, "numDevices": 5000, "startTimestamp": "2020-01-01 00:00:00", 
+            "endTimestamp": "2020-04-01 00:00:00", "minLat": 40.0, "maxLat": 43.0, "minLon": -93.0, "maxLon": -89.0,
+            "generateWkt": False}),
+        ("basic/telematics", 
+            {"rows": 1000, "partitions": 4, "random": True, "numDevices": 1000, "startTimestamp": "2020-01-01 00:00:00", 
+            "endTimestamp": "2020-04-01 00:00:00", "minLat": 45.0, "maxLat": 35.0, "minLon": -89.0, "maxLon": -93.0,
+            "generateWkt": True}),
+        ("basic/telematics", 
+            {"rows": -1, "partitions": -1, "numDevices": 1000, "minLat": 98.0, "maxLat": 100.0, 
+            "minLon": -181.0, "maxLon": -185.0, "generateWkt": False}),
+        ("basic/telematics", 
+            {"rows": 5000, "partitions": -1, "startTimestamp": "2020-01-01 00:00:00", 
+            "endTimestamp": "2020-04-01 00:00:00", "generateWkt": True}),
+        ("basic/telematics", {}),
+        ("basic/telematics", 
+            {"rows": -1, "partitions": -1, "random": False, "numDevices": 50, "startTimestamp": "2020-06-01 00:00:00", 
+            "endTimestamp": "2020-04-01 00:00:00", "minLat": 40.0, "maxLat": 43.0, "minLon": -93.0, "maxLon": -89.0,
+            "generateWkt": False}),
+        ("basic/telematics", 
+            {"rows": -1, "partitions": -1, "random": False, "numDevices": 100, "startTimestamp": "2020-01-01 00:00:00",
+             "endTimestamp": "2020-04-01 00:00:00", "maxLat": 45.0, "minLon": -93.0, "generateWkt": False}),
+        ("basic/telematics", 
+            {"rows": -1, "partitions": -1, "random": False, "numDevices": 100, "startTimestamp": "2020-01-01 00:00:00",
+            "endTimestamp": "2020-04-01 00:00:00", "minLat": 45.0, "generateWkt": False}),
+        ("basic/telematics",
+            {"rows": -1, "partitions": -1, "random": False, "numDevices": 100, "startTimestamp": "2020-01-01 00:00:00",
+            "endTimestamp": "2020-04-01 00:00:00", "minLat": -120.0, "generateWkt": False}),
+        ("basic/telematics",
+            {"rows": -1, "partitions": -1, "random": False, "numDevices": 100, "startTimestamp": "2020-01-01 00:00:00",
+            "endTimestamp": "2020-04-01 00:00:00", "maxLat": -120.0, "generateWkt": False}),
+        ("basic/telematics",
+            {"rows": -1, "partitions": -1, "random": False, "numDevices": 100, "startTimestamp": "2020-01-01 00:00:00",
+            "endTimestamp": "2020-04-01 00:00:00", "minLon": 190.0, "generateWkt": False}),
+        ("basic/telematics",
+            {"rows": -1, "partitions": -1, "random": False, "numDevices": 100, "startTimestamp": "2020-01-01 00:00:00",
+            "endTimestamp": "2020-04-01 00:00:00", "maxLon": 190.0, "generateWkt": False}),
+    ])
+    def test_basic_telematics_retrieval(self, providerName, providerOptions):
+        ds = dg.Datasets(spark, providerName).get(**providerOptions)
+        assert ds is not None
+
+        df = ds.build()
+        assert df.count() >= 0
+
+        row = df.first().asDict()
+        assert "lat" in df.columns
+        assert "lon" in df.columns
+        assert "heading" in df.columns
+        assert df.where('heading < 0 and heading > 359').count() == 0
+
+        minLat = providerOptions.get("minLat", -90.0)
+        maxLat = providerOptions.get("maxLat", 90.0)
+        minLat = max(minLat, -90.0)
+        maxLat = min(maxLat, 90.0)
+        if minLat > 90.0:
+            minLat = 89.0
+        if maxLat < -90.0:
+            maxLat = -89.0
+        if minLat > maxLat:
+            (minLat, maxLat) = (maxLat, minLat)
+        assert df.where(f'lat < {minLat}').count() == 0
+        assert df.where(f'lat > {maxLat}').count() == 0
+
+        minLon = providerOptions.get("minLon", -180.0)
+        maxLon = providerOptions.get("maxLon", 180.0)
+        minLon = max(minLon, -180.0)
+        maxLon = min(maxLon, 180.0)
+        if minLon > 180.0:
+            minLon = 179.0
+        if maxLon < -180.0:
+            maxLon = -179.0
+        if minLon > maxLon:
+            (minLon, maxLon) = (maxLon, minLon)
+        assert df.where(f'lon < {minLon}').count() == 0
+        assert df.where(f'lon > {maxLon}').count() == 0
+
+        startTimestamp = providerOptions.get("startTimestamp", "2024-01-01 00:00:00")
+        endTimestamp = providerOptions.get("endTimestamp", "2024-02-01 00:00:00")
+        if startTimestamp > endTimestamp:
+            (startTimestamp, endTimestamp) = (endTimestamp, startTimestamp)
+        assert df.where(f'ts < "{startTimestamp}"').count() == 0
+        assert df.where(f'ts > "{endTimestamp}"').count() == 0
+
+        generateWkt = providerOptions.get("generateWkt", False)
+        if generateWkt:
+            assert "wkt" in row.keys()
+
+        random = providerOptions.get("random", None)
+        if random:
+            print("")
+            leadingRows = df.limit(100).collect()
+            ids = [r.device_id for r in leadingRows]
+            assert ids != sorted(ids)
+
+    # BASIC USER tests:
     @pytest.mark.parametrize("providerName, providerOptions", [
         ("basic/user", {"rows": 50, "partitions": 4, "random": False, "dummyValues": 0}),
         ("basic/user", {"rows": -1, "partitions": 4, "random": False, "dummyValues": 0}),
@@ -24,12 +227,46 @@ class TestStandardDatasetProviders:
 
         assert df.count() >= 0
 
-        if 'random' in providerOptions and providerOptions['random']:
+        random = providerOptions.get("random", None)
+        if random:
             print("")
             leadingRows = df.limit(100).collect()
             customer_ids = [r.customer_id for r in leadingRows]
             assert customer_ids != sorted(customer_ids)
 
+    # BENCHMARK GROUPBY tests:
+    @pytest.mark.parametrize("providerName, providerOptions", [
+        ("benchmark/groupby", {"rows": 50, "partitions": 4, "random": False, "groups": 10, "percentNulls": 0.1}),
+        ("benchmark/groupby", {"rows": -1, "partitions": 4, "random": True, "groups": 100}),
+        ("benchmark/groupby", {}),
+        ("benchmark/groupby", {"rows": 1000, "partitions": -1, "random": False}),
+        ("benchmark/groupby", {"rows": -1, "groups": 1000, "percentNulls": 0.2}),
+        ("benchmark/groupby", {"rows": 1000, "partitions": -1, "random": True, "groups": 5000, "percentNulls": 0.5}),
+        ("benchmark/groupby", {"rows": -1, "partitions": -1, "random": True, "groups": 0}),
+        ("benchmark/groupby", {"rows": 10, "partitions": -1, "random": True, "groups": 100, "percentNulls": 0.1}),
+        ("benchmark/groupby", {"rows": -1, "partitions": -1, "random": False, "groups": -50}),
+        ("benchmark/groupby", {"rows": -1, "partitions": -1, "random": False, "groups": -50, "percentNulls": -12.1}),
+        ("benchmark/groupby", {"rows": -1, "partitions": -1, "random": True, "groups": -50, "percentNulls": 1.1}),
+    ])
+    def test_benchmark_groupby_retrieval(self, providerName, providerOptions):
+        ds = dg.Datasets(spark, providerName).get(**providerOptions)
+        assert ds is not None
+
+        df = ds.build()
+        assert df.count() >= 0
+
+        percentNulls = providerOptions.get("percentNulls", 0.0)
+        if percentNulls >= 1.0:
+            return
+
+        random = providerOptions.get("random", None)
+        if random:
+            print("")
+            leadingRows = df.limit(100).collect()
+            vals = [r.v3 for r in leadingRows]
+            assert vals != sorted(vals)
+
+    # MULTI-TABLE TELEPHONY tests:
     @pytest.mark.parametrize("providerName, providerOptions", [
         ("multi_table/telephony", {"rows": 50, "partitions": 4, "random": False}),
         ("multi_table/telephony", {"rows": -1, "partitions": 4, "random": False}),
